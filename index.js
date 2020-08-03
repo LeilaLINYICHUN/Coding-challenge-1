ed14daf96719e99035d13b51f896cd6e7f469873
leilalyc <lin.yc928@gmail.com>
2020-08-03 16:14:22 -0500

    Coding challenge 1

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 const shareIcon = document.querySelector('.share');
-console.log(shareIcon);
-// // shareButton.addEventListener('click', (event) => {
-// //   event.target.style.fill = 'hsl(210, 46%, 95%)';
-// // });
-
 const modal = document.querySelector('.popup');
 const button = document.querySelector('.circle');
 
 button.addEventListener('click', openModal);
-//shareIcon.addEventListener('click', openModal);
 
 function openModal() {
   modal.style.display = 'block';
-  shareIcon.style.stroke = 'hsl(210, 46%, 95%)';
+  shareIcon.firstChild.style.fill = 'hsl(210, 46%, 95%)';
   button.style.backgroundColor = 'hsl(214, 17%, 51%)';
 }
-
-// button.addEventListener('click', (open) => {
-//   event.target.style.backgroundColor = 'hsl(214, 17%, 51%)';
-//   event.target.style.fill = 'hsl(210, 46%, 95%)';
-// });
-
-// const toggleModal = () => {
-//   document.querySelector('.circle').classList.toggle('popup--hidden');
-// };
-
-// document.querySelector('.popup'), addEventListener('click', toggleModal);
-
-//'hsl(214, 17%, 51%)'
